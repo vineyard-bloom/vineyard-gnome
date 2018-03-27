213c424ac57b00b6eec9352e473b3afc4d643081
Adam Clark <adamclark@Adams-MacBook-Pro.local>
2018-03-27 09:30:59 -0600

    add test data ts files

---
+++ test/tests/testData2.ts
@@ -0,0 +1,18 @@
+module.exports = {
+ fsoo: {
+  baz: 'bar'
+ },
+ status: "localhost",
+ goal: "ie",
+ result: [
+  {
+   hash: "0x3505799c9749cfa7f1c45cd8888d6c001c257a4a34713b46e92bdc576e95b3cf",
+  },
+  {
+   hashh: "foo",
+  },
+  {
+   test: 'trve'
+  }
+ ]
+};
